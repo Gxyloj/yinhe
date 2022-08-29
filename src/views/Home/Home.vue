1616ff9e7cd75d9e5107f4f68831616a7c1506a4
Gxyloj <306834107@qq.com>
2022-08-29 10:21:30 +0800

    init

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="home">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+}
+</script>
+
+<style scoped lang="scss">
+#home{
+  padding: 10px;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {'process.env': {}},
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "__ROOT__": path.resolve(__dirname, ""),
+      "comps": path.resolve(__dirname, "src/components"),
+    }
+  },
+})
+
